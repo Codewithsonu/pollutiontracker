829c2a06676d8543a13baba50b74b479db1718f5
sesetti jaswanth <sesettijaswanth@gmail.com>
2023-04-20 12:21:50 +0000

    Details view and expend of the view is working

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,13 +1,15 @@
 sap.ui.define(
     [
-        "sap/ui/core/mvc/Controller"
+        "sap/ui/core/mvc/Controller",
+        "sap/ui/model/json/JSONModel",
+        "sap/ui/webc/fiori/library"
     ],
-    function(BaseController) {
+    function(BaseController,JSONModel,fiorilibrary,FlexibleColumnLayout) {
       "use strict";
   
       return BaseController.extend("com.sap.pollutiontracker.controller.Detail", {
         onInit() {
-          
+
           var countriesModel = new JSONModel('https://4c45935f-f293-42b8-8e32-3cfc24b6ca46.mock.pstmn.io/countries/');
           countriesModel.setSizeLimit(1000);
           //setting the countriesModel to countries model
--- webapp/controller/Master.controller.js
+++ webapp/controller/Master.controller.js
@@ -1,33 +1,40 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
-], function(Controller,JSONModel) {
-	"use strict";
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/f/library",
+    "sap/f/LayoutType",
+    "sap/ui/webc/fiori/library",
+    "sap/suite/ui/commons/networkgraph/layout/TwoColumnsLayout"
+], function(Controller, JSONModel, library, LayoutType, fioriLibrary, TwoColumnsLayout) {
+    "use strict";
 
-	return Controller.extend("com.sap.pollutiontracker.controller.Master", {
+    return Controller.extend("com.sap.pollutiontracker.controller.Master", {
         /**
          * @override
          */
         onInit: function() {
 
-            console.log("the Pollution tracker App oninit function is runing ");
-                // featching the data from the API link 
-                var countriesModel = new JSONModel('https://4c45935f-f293-42b8-8e32-3cfc24b6ca46.mock.pstmn.io/countries/');
-                countriesModel.setSizeLimit(1000);
-                //setting the countriesModel to countries model
-                this.getView().setModel(countriesModel,'countries');
-                
-                var countriesModelData = this.getView().getModel('countries')
-                console.log(countriesModelData);
-            
-        
+            console.log("the Pollution tracker App oninit function is running ");
+            // fetching the data from the API link 
+            var countriesModel = new JSONModel('https://4c45935f-f293-42b8-8e32-3cfc24b6ca46.mock.pstmn.io/countries/');
+            countriesModel.setSizeLimit(1000);
+            //setting the countriesModel to countries model
+            this.getView().setModel(countriesModel, 'countries');
+
+            var countriesModelData = this.getView().getModel('countries')
+            console.log(countriesModelData);
         },
-        OnCountryPress:function(oEvent){
-            console.log("On country Press ");
-            
+
+        OnCountryPress: function(oEvent) {
+
+            console.log("On country Press  event is triggered ");
+
+            var getFCLRef = this.getView().getParent().getParent();
+
+            getFCLRef.setLayout(LayoutType.TwoColumnsMidExpanded);
+
+        },
 
 
-        }
-
-	});
+    });
 });
